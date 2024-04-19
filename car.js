dae4f68188eeaa13c0d47c92c0d93d3c5d626199
NicoG <winnipeg.francois@gmail.com>
2024-04-19 21:42:29 +0200

    Création setup, route et voiture

---
+++ car.js
@@ -0,0 +1,20 @@
+class Car{
+
+	constructor(x, y, width, height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	draw(ctx){
+		ctx.beginPath();
+		ctx.rect(
+			this.x-this.width/2.0,
+			this.y-this.height/2.0,
+			this.width,
+			this.height
+		);
+		ctx.fill();
+	}
+}
