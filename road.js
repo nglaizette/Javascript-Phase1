b381ba56b78e0908b527fde8f220a85f67580fbb
NicoG <winnipeg.francois@gmail.com>
2024-04-20 22:19:43 +0200

    Creation du nombre de voies de circulaton

--- road.js
+++ road.js
@@ -16,14 +16,21 @@ class Road {
 		ctx.lineWidth = 5;
 		ctx.strokeStyle = "white";
 
-		ctx.beginPath();
-		ctx.moveTo(this.left, this.top);
-		ctx.lineTo(this.left,this.bottom);
-		ctx.stroke();
-
-		ctx.beginPath();
-		ctx.moveTo(this.right, this.top);
-		ctx.lineTo(this.right,this.bottom);
-		ctx.stroke();
+		for(let i=0; i <= this.laneCount; i++){
+			const x = lerp(
+				this.left,
+				this.right,
+				i/this.laneCount
+			);
+			ctx.beginPath();
+			ctx.moveTo(x, this.top);
+			ctx.lineTo(x,this.bottom);
+			ctx.stroke();
+		}
 	}
+}
+
+// linear intertrapolation
+function lerp(A, B,t) {
+	return A+(B-A)*t;
 }
