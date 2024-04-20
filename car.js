12c84ef17a6324614bf3689c40b4f099a58c4dab
NicoG <winnipeg.francois@gmail.com>
2024-04-20 22:02:14 +0200

    Refactoring de la méthode update

--- car.js
+++ car.js
@@ -16,50 +16,50 @@ class Car{
 	}
 
 	update(){
-		// voir box2D.js pour une autre physique
-		if(this.controls.forward){
-			this.speed += this.acceleration;
-		}
-		if(this.controls.reverse){
-			this.speed -= this.acceleration;
-		}
+		this.#move();
+	}
 
-		if(this.speed > this.maxSpeed){
-			this.speed= this.maxSpeed;
-		}
-
-		if(this.speed<-this.maxSpeed/2.0){
-			this.speed=-this.maxSpeed/2.0;
-		}
-
-		if(this.speed > 0){
-			this.speed -=this.friction;
-		}
-		if(this.speed <0){
-			this.speed+=this.friction;
-		}
-
-		if(Math.abs(this.speed) < this.friction){
-			this.speed = 0;
-		}
-
-		if(this.speed != 0){
-			const flip = this.speed> 0? 1: -1;
-			
-			if(this.controls.left){
-				this.angle+=0.03 * flip;
-			}
-
-			if(this.controls.right){
-				this.angle-=0.03 * flip;
-			}
-		}
-
-
-
-
-		this.x -= Math.sin(this.angle)*this.speed;
-		this.y -= Math.cos(this.angle)*this.speed;
+	#move(){
+				// voir box2D.js pour une autre physique
+				if(this.controls.forward){
+					this.speed += this.acceleration;
+				}
+				if(this.controls.reverse){
+					this.speed -= this.acceleration;
+				}
+		
+				if(this.speed > this.maxSpeed){
+					this.speed= this.maxSpeed;
+				}
+		
+				if(this.speed<-this.maxSpeed/2.0){
+					this.speed=-this.maxSpeed/2.0;
+				}
+		
+				if(this.speed > 0){
+					this.speed -=this.friction;
+				}
+				if(this.speed <0){
+					this.speed+=this.friction;
+				}
+		
+				if(Math.abs(this.speed) < this.friction){
+					this.speed = 0;
+				}
+		
+				if(this.speed != 0){
+					const flip = this.speed> 0? 1: -1;
+					
+					if(this.controls.left){
+						this.angle+=0.03 * flip;
+					}
+		
+					if(this.controls.right){
+						this.angle-=0.03 * flip;
+					}
+				}
+				this.x -= Math.sin(this.angle)*this.speed;
+				this.y -= Math.cos(this.angle)*this.speed;
 	}
 
 	draw(ctx){
