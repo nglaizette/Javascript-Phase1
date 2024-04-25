906ec0cfb742b84de69430516a767e01a54703e4
NicoG <winnipeg.francois@gmail.com>
2024-04-25 23:19:46 +0200

    Ajout fonction sauvegarde et suppresion du meilleur cerveau et leur boutons

--- main.js
+++ main.js
@@ -11,12 +11,26 @@ const road = new Road(carCanvas.width/2.0, carCanvas.width * 0.9);
 
 const N=100;
 const cars = generateCars(N);
+let bestCar = cars[0];
+
+if(localStorage.getItem("bestBrain")){
+	bestCar.brain=JSON.parse(
+		localStorage.getItem("bestBrain"));
+}
 
 const traffic=[
 	new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2.0),
 ]
 
 animate()
+
+function save(){
+	localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+
+function discard(){
+	localStorage.removeItem("bestBrain");
+}
 
 function generateCars(N) {
 	const cars = [];
@@ -36,7 +50,7 @@ function animate(time) {
 	}
 
 	// recherche de la voiture dont le y est le minimum de toutes les voitures
-	const bestCar = cars.find(
+	bestCar = cars.find(
 		c => c.y==Math.min(...cars.map(c=>c.y))
 	);
 	
