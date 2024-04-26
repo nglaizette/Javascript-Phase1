83ba2495b1eb14df5807975b448ba44fc5ad13c7
NicoG <winnipeg.francois@gmail.com>
2024-04-26 18:27:40 +0200

    Ajout de couleur au hasard pour les voitures non jouées

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width/2.0, carCanvas.width * 0.9);
 
-const N=10 ;
+const N=10;
 const cars = generateCars(N);
 let bestCar = cars[0];
 
@@ -25,13 +25,13 @@ if(localStorage.getItem("bestBrain")){
 }
 
 const traffic=[
-	new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2.0),
-	new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2.0),
-	new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2.0),
-	new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2.0),
-	new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2.0),
-	new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2.0),
-	new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2.0),
+	new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2.0, getRandomColor()),
+	new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2.0, getRandomColor()),
+	new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2.0, getRandomColor()),
+	new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2.0, getRandomColor()),
+	new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2.0, getRandomColor()),
+	new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2.0, getRandomColor()),
+	new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2.0, getRandomColor()),
 ]
 
 animate()
