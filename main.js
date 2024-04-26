265033f656500af338a69b7204b3d371c36e6a97
NicoG <winnipeg.francois@gmail.com>
2024-04-26 12:09:48 +0200

    Correction génération voiture, ajout de nouvelles voitures supplémentaires, affichage de la meilleure voiture

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width/2.0, carCanvas.width * 0.9);
 
-const N=100;
+const N=1;
 const cars = generateCars(N);
 let bestCar = cars[0];
 
@@ -18,7 +18,7 @@ if(localStorage.getItem("bestBrain")){
 		cars[i].brain=JSON.parse(localStorage.getItem("bestBrain"));
 
 		if(i!=0){
-			NeuralNetwork.mutate(cars[i].brain, 0.1);
+			NeuralNetwork.mutate(cars[i].brain, 0.15);
 		}
 	}
 
@@ -28,6 +28,10 @@ const traffic=[
 	new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2.0),
 	new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2.0),
 	new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2.0),
+	new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2.0),
+	new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2.0),
+	new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2.0),
+	new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2.0),
 ]
 
 animate()
@@ -42,7 +46,7 @@ function discard(){
 
 function generateCars(N) {
 	const cars = [];
-	for(let i=1; i < N; i++){
+	for(let i=0; i < N; i++){
 		cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
 	}
 	return cars;
