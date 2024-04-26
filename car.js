7f65901663de3a0f4ce04075ea1a4d7085fdd4c5
NicoG <winnipeg.francois@gmail.com>
2024-04-26 18:18:47 +0200

    Bidouille dans la fonction draw de Car pour avoir une image supperposée à un fond coloré

--- car.js
+++ car.js
@@ -1,6 +1,6 @@
 class Car{
 
-	constructor(x, y, width, height, controlType, maxSpeed=3){
+	constructor(x, y, width, height, controlType, maxSpeed=3, color="blue"){
 		this.x = x;
 		this.y = y;
 		this.width = width;
@@ -26,6 +26,21 @@ class Car{
 
 		this.img = new Image();
 		this.img.src = "car.png";
+
+		this.mask = document.createElement("canvas");
+		this.mask.width = width;
+		this.mask.height = height;
+
+		const maskCtx = this.mask.getContext("2d");
+		this.img.onload=()=> {
+			maskCtx.fillStyle=color;
+			maskCtx.rect(0,0, this.width, this.height);
+			maskCtx.fill();
+
+			maskCtx.globalCompositeOperation="destination-atop";
+			maskCtx.drawImage(this.img, 0, 0, this.width, this.height);
+
+		}
 	}
 
 	update(roadBorders, traffic) {
@@ -137,15 +152,21 @@ class Car{
 				this.y -= Math.cos(this.angle)*this.speed;
 	}
 
-	draw(ctx, color, drawSensor=false){
+	draw(ctx, drawSensor=false){
 
 		ctx.save();
 		ctx.translate(this.x, this.y);
-		ctx.drawImage(this.img,
+		ctx.drawImage(this.mask,
 			-this.width/2.0,
 			-this.height/2.0,
 			this.width,
 			this.height);
+		ctx.globalCompositeOperation="multiply";
+		ctx.drawImage(this.img,
+				-this.width/2.0,
+				-this.height/2.0,
+				this.width,
+				this.height);
 		ctx.restore();
 
 		if(this.sensor && drawSensor){
